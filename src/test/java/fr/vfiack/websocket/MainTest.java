a20e90c68e2f8eb5e9657de322e4bdc763ce2274
Vincent Fiack <vfiack@gmail.com>
2020-12-15 10:44:43 +0100

    simulate fixing broken test

--- src/test/java/fr/vfiack/websocket/MainTest.java
+++ src/test/java/fr/vfiack/websocket/MainTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
     @Test
-    public void failure() {
-        fail("Testing junit failures");
+    public void sucess() {
+        assertTrue(true);
     }
 }
