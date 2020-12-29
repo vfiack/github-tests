2b3a901791ea6e6f872a209e5c1b908d06e361fb
Vincent Fiack <vfiack@gmail.com>
2020-12-29 11:35:01 +0100

    convert to multimodule project

---
+++ secondary/src/main/java/CheckSonarOptionalSupport.java
@@ -0,0 +1,17 @@
+import java.util.List;
+import java.util.Optional;
+
+public class CheckSonarOptionalSupport {
+    public static int checkMe() {
+        Optional<Integer> optional = List.of(1, 2, 3).stream().filter(i -> i > 2)
+                .findFirst();
+
+        if(optional.isEmpty()) {
+            return 0;
+        }
+
+        // will sonar complain about accessing optional without is present call?
+        return 2 * optional.get();
+    }
+
+}
