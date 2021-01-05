1de398beee846c4e5571f0bed9645bc230a22106
Vincent Fiack <vfiack@gmail.com>
2021-01-05 09:47:34 +0100

    full analysis on merge

--- .github/workflows/actions-sonar-modules.sh
+++ .github/workflows/actions-sonar-modules.sh
@@ -4,13 +4,20 @@
 # contains space-separated paths for all changed files
 INPUT=$HOME/files.txt
 
+# when the input does not exist, it means that file-change-actions was not executed
+# this is normal for push triggers - in this case, analyse all modules
+if [ ! -f $INPUT ]; then
+  echo "*** ANALYZING all modules ***"
+  ls > $INPUT
+fi
+
 # split files by space then extract and deduplicate their root folder
 # filter those containing a pom.xml file
 # and finally run sonar on those
-cat $HOME/files.txt | tr " " "\n" | cut -d '/' -f 1 | sort | uniq | while read MODULE; do
+cat $INPUT | tr " " "\n" | cut -d '/' -f 1 | sort | uniq | while read MODULE; do
 	POM=$MODULE/pom.xml
 	if [ -f $POM ]; then
-		echo "*** ANALYZING " $MODULE
+		echo "*** ANALYZING " $MODULE " ***"
 		mvn -f $POM -B org.sonarsource.scanner.maven:sonar-maven-plugin:sonar
 	fi
 done
