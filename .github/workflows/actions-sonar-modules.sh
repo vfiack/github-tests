473b02c49ad7babcd6a85d1bb44c575357104b21
Vincent Fiack <vfiack@gmail.com>
2021-01-05 09:23:18 +0100

    try again

--- .github/workflows/actions-sonar-modules.sh
+++ .github/workflows/actions-sonar-modules.sh
@@ -1,16 +1,22 @@
 #!/bin/sh
 
+echo "listing home files"
+ls $HOME
+
+echo "listing current files"
+ls
+
+echo "cat files.txt"
+cat $HOME/files.txt
+
 # get all changed files from command line argument
 # then extract and deduplicate their root folder
 # filter those containing a pom.xml file
 # and finally run sonar on those
-for FILE in $*; do
-	echo $FILE | cut -d '/' -f 1
-done | sort | uniq | while read MODULE; do
+cat $HOME/files.txt | cut -d '/' -f 1 | sort | uniq | while read MODULE; do
 	POM=$MODULE/pom.xml
 	if [ -f $POM ]; then
-		echo analyzing $MODULE
+		echo "*** ANALYZING " $MODULE
 		mvn -f $POM -B org.sonarsource.scanner.maven:sonar-maven-plugin:sonar
 	fi
 done
-
